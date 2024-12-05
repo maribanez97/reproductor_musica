16e44788adafd765d5bbb6ca853c6a5a30798cd3
maribanez97 <maria.ibanez1997@alumnos.cei.es>
2024-12-05 10:36:36 +0100

    update

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 const listaCanciones = ["Normal", "Mi Luz", "Guerrera", "Cangrinaje", "Ingobernable"];
 const listaArtistas = ["Feid", "Rels B", "DELLAFUENTE", "Cruz Cafuné", "El Madrileño"];
 const divCanciones = document.querySelector("#ListaCanciones")
-const pArtistas = document.querySelector("#ListaArtistas")
+
+// Defino botones
+const btnCancion3 = document.querySelector("#BtnCancion3")
+const btnAnterior = document.querySelector("#BtnAnterior")
+const btnPlay = document.querySelector("#BtnPlay")
+const btnSiguiente = document.querySelector("#BtnSiguiente")
+
 
         
 listaCanciones.forEach((cancion,i)=>{
@@ -12,6 +18,43 @@ listaCanciones.forEach((cancion,i)=>{
         ${listaCanciones[i]} - ${listaArtistas[i]}
                                    </div>`   
 });
+
+btnCancion3.addEventListener('click', ()=>{
+    console.log(`El tema 3 es: ${listaCanciones[3]} de ${listaArtistas[3]}`);
+});
+
+let cantCanciones = listaCanciones.length;
+let cantArtistas = listaArtistas.length;
+
+btnAnterior.addEventListener('click', (event) => {
+    console.log(event.target.id);
+    // console.log(cantCanciones);
+    // console.log(cantArtistas);
+    cantArtistas = cantArtistas-1;
+    cantCanciones = cantCanciones-1;
+    console.log(`Canción anterior ${listaCanciones[cantCanciones]} de ${listaArtistas[cantArtistas]}`);
+});
+
+btnPlay.addEventListener('click', (event) => {
+    console.log(event.target.id);
+    // console.log(cantCanciones);
+    // console.log(cantArtistas);
+    // cantArtistas = cantArtistas+1;
+    // cantCanciones = cantCanciones+1;
+    console.log(`Canción siguiente ${listaCanciones[cantCanciones]} de ${listaArtistas[cantArtistas]}`);
+});
+
+btnSiguiente.addEventListener('click', (event) => {
+    console.log(event.target.id);
+    // console.log(cantCanciones);
+    // console.log(cantArtistas);
+    cantArtistas = cantArtistas+1;
+    cantCanciones = cantCanciones+1;
+    console.log(`Canción siguiente ${listaCanciones[cantCanciones]} de ${listaArtistas[cantArtistas]}`);
+});
+
+
+
 
 
 
