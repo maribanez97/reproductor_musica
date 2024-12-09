88b357c248f6944f4369ef890617cd283af58e65
maribanez97 <maria.ibanez1997@alumnos.cei.es>
2024-12-09 10:49:08 +0100

    update

--- javascript.js
+++ javascript.js
@@ -5,9 +5,11 @@ const divCanciones = document.querySelector("#ListaCanciones")
 // Defino botones
 const btnCancion3 = document.querySelector("#BtnCancion3")
 const btnAnterior = document.querySelector("#BtnAnterior")
-const btnPlay = document.querySelector("#BtnPlay")
+const btnPlayPausa = document.querySelector("#BtnPlayPausa")
+// const btnPausa = document.querySelector("#BtnPausa")
 const btnSiguiente = document.querySelector("#BtnSiguiente")
 
+let idCancionActual = 0; // pimera cancion
 
         
 listaCanciones.forEach((cancion,i)=>{
@@ -21,67 +23,74 @@ listaCanciones.forEach((cancion,i)=>{
 
 
 
-btnCancion3.addEventListener('click', ()=>{
-    console.log(`El tema 3 es: ${listaCanciones[2]} de ${listaArtistas[2]}`); // índice 2 porque lalista empieza en índice 0
-});
+// btnCancion3.addEventListener('click', ()=>{
+//     console.log(`El tema 3 es: ${listaCanciones[2]} de ${listaArtistas[2]}`); // índice 2 porque lalista empieza en índice 0
+
+//     imprimiReproduciendo(2);
+// });
 
 // document.addEventListener('click', (event) => {
 //     console.log(event.target.id);
 // });
 
-// buscamos en nuestro HTML todos los divs con class Lista
+// Buscamos en nuestro HTML todos los divs con class Lista
 const divsCanciones = document.querySelectorAll(".Lista-cancion");
 const divPlayingSong = document.getElementById("PlayingSong");
 
 divsCanciones.forEach((divCancion, i) => {
     divCancion.addEventListener("click", () => {
-        console.log(`${listaCanciones[i]} - ${listaArtistas[i]}`);
-        // canción reproduciendose
-        divPlayingSong.innerHTML =  `<div> 
-                                    ${listaCanciones[i]}<br>
-                                    ${listaArtistas[i]}
-                                   </div>` 
+        
+        idCancionActual = i;
+        imprimiReproduciendo();
     });
 });
 
-// function inprimiReproduciendose((indice) =>{
-//     console.log(`${listaCanciones[indice]} - ${listaArtistas[indice]}`);
-//         // canción reproduciendose
-//         divPlayingSong.innerHTML =  `<div> 
-//                                     ${listaCanciones[i]}<br>
-//                                     ${listaArtistas[i]}
-//                                    </div>`;
-// });
+//-------------------------------------------
+// Defino la función paraimprimir la canción en pantalla
+//-------------------------------------------
+function imprimiReproduciendo(){
+    console.log(`${listaCanciones[idCancionActual]} - ${listaArtistas[idCancionActual]}`);
+        // canción reproduciendose
+        divPlayingSong.innerHTML =  `<div> 
+                                    ${listaCanciones[idCancionActual]}<br>
+                                    ${listaArtistas[idCancionActual]}
+                                   </div>`;
+};
 
-let cantCanciones = listaCanciones.length;
-let cantArtistas = listaArtistas.length;
-let idCancionActual = 0; // pimera cancion
+
 
 btnAnterior.addEventListener('click', () => {
-    // console.log(event.target.id);
-    // console.log(cantCanciones);
-    // console.log(cantArtistas);
-    cantArtistas = cantArtistas-1;
-    cantCanciones = cantCanciones-1;
-    console.log(`Canción anterior ${listaCanciones[cantCanciones]} de ${listaArtistas[cantArtistas]}`);
+    if (idCancionActual>0){
+            idCancionActual--;
+            imprimiReproduciendo ();
+    } else{
+            idCancionActual=4;
+            console.log(idCancionActual);
+            imprimiReproduciendo ();
+    }
 });
 
-btnPlay.addEventListener('click', () => {
-    // console.log(event.target.id);
-    // console.log(cantCanciones);
-    // console.log(cantArtistas);
-    // cantArtistas = cantArtistas+1;
-    // cantCanciones = cantCanciones+1;
-    console.log(`Canción siguiente ${listaCanciones[cantCanciones]} de ${listaArtistas[cantArtistas]}`);
+btnPlayPausa.addEventListener('click', () => {
+
+    btnPlayPausa.classList.toggle('fa-play');
+    
+
+    // imprimiReproduciendo ();
 });
+
+// btnPausa.addEventListener('click', () => {
+//     imprimiReproduciendo ();
+// });
 
 btnSiguiente.addEventListener('click', () => {
-    // console.log(event.target.id);
-    // console.log(cantCanciones);
-    // console.log(cantArtistas);
-    cantArtistas = cantArtistas+1;
-    cantCanciones = cantCanciones+1;
-    console.log(`Canción siguiente ${listaCanciones[cantCanciones]} de ${listaArtistas[cantArtistas]}`);
+        if (idCancionActual<4){
+            idCancionActual++;
+            imprimiReproduciendo ();
+    } else{
+            idCancionActual=0;
+            console.log(idCancionActual);
+            imprimiReproduciendo ();
+    }
 });
 
 
